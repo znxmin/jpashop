32171f7786c965e032127f9aae5521a808a000f3
limminjeong <limminjeong98@naver.com>
2023-04-29 15:10:23 +0900

    홈 화면과 레이아웃

--- src/main/java/jpabook/jpashop/controller/HomeController.java
+++ src/main/java/jpabook/jpashop/controller/HomeController.java
@@ -1,2 +1,18 @@
-package jpabook.jpashop.controller;public class HomeController {
+package jpabook.jpashop.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@Slf4j
+public class HomeController {
+
+
+    @RequestMapping(value = "/")
+    public String home() {
+        log.debug("home controller");
+        return "home";
+    }
 }
