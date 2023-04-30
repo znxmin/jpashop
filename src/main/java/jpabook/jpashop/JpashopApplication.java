52224e85c0261a90b20e089a047bbe533752539e
limminjeong <limminjeong98@naver.com>
2023-04-30 21:51:35 +0900

    프록시 객체를 json으로 리턴하기 위해 Hibernate5JakartaModule 빈 등록

--- src/main/java/jpabook/jpashop/JpashopApplication.java
+++ src/main/java/jpabook/jpashop/JpashopApplication.java
@@ -1,7 +1,9 @@
 package jpabook.jpashop;
 
+import com.fasterxml.jackson.datatype.hibernate5.jakarta.Hibernate5JakartaModule;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class JpashopApplication {
@@ -10,4 +12,14 @@ public class JpashopApplication {
 		SpringApplication.run(JpashopApplication.class, args);
 	}
 
+	// OrderSimpleApiController - /api/v1/simple-orders
+	// 초기화된 Proxy 객체만 노출하고, 초기화되지 않은 프록시 객체는 노출하지 않도록 설정
+	@Bean
+	Hibernate5JakartaModule hibernate5Module() {
+		// 강제 지연 로딩 설정 필요시 아래 설정 적용
+		// Hibernate5JakartaModule hibernate5JakartaModule = new Hibernate5JakartaModule();
+		// hibernate5JakartaModule.configure(Hibernate5JakartaModule.Feature.FORCE_LAZY_LOADING, true);
+		// return hibernate5JakartaModule;
+		return new Hibernate5JakartaModule();
+	}
 }
